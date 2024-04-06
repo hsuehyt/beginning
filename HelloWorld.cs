2c3f22e3365f60232631451978d227a124a857e6
Yuting Hsueh <hsueh.yuting@gmail.com>
2024-04-06 23:17:18 +0800

    public string firstName;

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HelloWorld : MonoBehaviour
 {
+    public string firstName;
     // Start is called before the first frame update
     void Start()
     {
