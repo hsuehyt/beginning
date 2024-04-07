2eceed5f8232a999f02a1848e14eb3335ddab058
Yuting Hsueh <hsueh.yuting@gmail.com>
2024-04-07 09:54:13 +0800

    last name

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -5,18 +5,22 @@ using TMPro;
 
 public class HelloWorld : MonoBehaviour
 {
-    public string firstName;
-    private TextMeshProUGUI textMeshPro;
+    public string firstName; // Existing public variable for the first name.
+    public string lastName;  // Added public variable for the last name.
+
+    private TextMeshProUGUI textMeshPro; // Private variable to hold the TextMeshProUGUI component.
+
     // Start is called before the first frame update
     void Start()
     {
-        textMeshPro = GetComponent<TextMeshProUGUI>();
-        textMeshPro.text = $"Hello {firstName}!";
+        textMeshPro = GetComponent<TextMeshProUGUI>(); // Get the TextMeshProUGUI component.
+        // Update the text to include both the first name and last name.
+        textMeshPro.text = $"Hello {firstName} {lastName}!";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // This method is left empty as before.
     }
 }
